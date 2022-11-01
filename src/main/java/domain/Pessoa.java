4dbc6a705d4a86e1841d46ca9e0c1a0e2373ddf8
AleehSophia <alessia.casado@gmail.com>
2022-11-01 15:14:37 -0300

    feat: mapeamento

--- src/main/java/domain/Pessoa.java
+++ src/main/java/domain/Pessoa.java
@@ -1,9 +1,14 @@
 package domain;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
 public class Pessoa implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     private String email;
